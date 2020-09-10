422d86660b8bb757bdb1ac4a9741352e00e5ae21
KeunHyeong <rmsgudxx@gmail.com>
2020-09-10 14:07:09 +0900

    Revert "Revert "try""
    This reverts commit d6b4747e25795a044142628255fa1d045aced0c8.

---
+++ LookInBody/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  LookInBody
+//
+//  Created by Apple on 2020/09/08.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
